72f1cf0cb44ff823aea9335456805c865bdf501a
Ensar DUMAN <dumanensar@hotmail.com>
2020-03-26 23:31:20 +0300

    return chain from api

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from backend.blockchain.blockchain import Blockchain
 
 app = Flask(__name__)
 
 @app.route('/')
 def default():
     return 'Welcome to the blockchain'
+
+blockchain = Blockchain()
+
+for i in range(3):
+    blockchain.add_block(i)
+
+@app.route('/blockchain')
+def route_blockchain():
+    return blockchain.__repr__()
 
 app.run(port=5000)
