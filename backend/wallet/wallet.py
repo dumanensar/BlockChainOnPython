da4bec90ca61c0e8440b456fe832ea5576be12d6
Ensar DUMAN <dumanensar@hotmail.com>
2020-03-29 16:21:50 +0300

    wallet sign function developed

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -1,8 +1,11 @@
 
+import json
 import uuid
+
 from backend.config import STARTING_BALANCE
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import hashes
 
 class Wallet:
     """
@@ -19,11 +22,22 @@ class Wallet:
             default_backend()
         )
         self.public_key = self.private_key.public_key()
+
+    def sign(self, data):
+        """
+        Generate a signature based on the data using the local private key 
+        """
+        json_data = json.dumps(data).encode('utf-8')
+        return self.private_key.sign(json_data, ec.ECDSA(hashes.SHA256()))
         
 
 def main():
     wallet = Wallet()
     print(f'wallet.__dict__: {wallet.__dict__}')
+    
+    data = {'foo': 'bar'}
+    signature = wallet.sign(data)
+    print(f'signature: {signature}')
 
 if __name__ == '__main__':
     main()
